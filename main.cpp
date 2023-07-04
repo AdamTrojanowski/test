53b9a2e4179d3370cb11dee9d14c9bc047d491ed
AdamTrojanowski <68925689+AdamTrojanowski@users.noreply.github.com>
2023-07-04 19:34:56 +0000

    matrix rotate

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    cout<<"Goodbye, world!"<<endl;
+vector<vector<int>> rotate(const vector<vector<int>>& g) {
+    int n = g.size();
+    vector<vector<int>> r(n, vector<int>(n));
+    for (int x = 0;x<n;x++) {
+        for (int y = 0;y < n; y++) {
+            r[x][y] = g[n - 1 - y][x];
+        }
+    }
+    return r;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<vector<int>> r(n, vector<int>(n));
+    for (int x = 0; x < n; x++) {
+        for (int y = 0; y < n; y++) {
+            cin >> r[x][y];
+        }
+    }
+    vector<vector<int>> g = rotate(r);
+    for (const auto& row : g) {
+        for (int height : row) {
+            cout << height << " ";
+        }
+        cout << endl;
+    }
 }
